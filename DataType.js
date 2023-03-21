69d52e74a4f4ca3a2a7a8399e743498e1ee5194e
Veronika <104579294+Nikavi@users.noreply.github.com>
2023-03-21 16:07:52 +0100

    Added function to height of customer
    Added a function that takes in a person's height and returns a recommended dress length based on that height.

--- DataType.js
+++ DataType.js
@@ -14,3 +14,15 @@ let dressFashion = [streightCut, freeCut, taperedCut];
 const dressSize = [xs, s, m, l, xl, xxl];
 const dressSizeWomen = [34, 36, 38, 40, 42, 44, 46, 48];
 const dressSizeMen = [44, 46, 48, 50, 52, 54];
+
+function recommendDressLength(height) {
+  if (height < 160) {
+    return dressLength[3]; // shortLength
+  } else if (height < 170) {
+    return dressLength[0]; // kneeLength
+  } else if (height < 180) {
+    return dressLength[2]; // averageLength
+  } else {
+    return dressLength[1]; // heelsLength
+  }
+}
