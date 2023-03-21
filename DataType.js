10f3ad72e2ae41082782e22fa8269430c0ef7ddc
Veronika <104579294+Nikavi@users.noreply.github.com>
2023-03-21 14:31:25 +0100

    Update DataType.js

--- DataType.js
+++ DataType.js
@@ -8,14 +8,9 @@ let measurementsThree = "measure around your seat";
 let dressLength = [kneeLength, heelsLength, averageLength, shortLength];
 let dressSleeveLength = [shortSleeves, mediumSleeves, longSleeves];
 
-let dressFabric = [cotton, viscose, silk, tencelLyocell, elastane];  ;
+let dressFabric = [cotton, viscose, silk, tencelLyocell, elastane];
 let dressFashion = [streightCut, freeCut, taperedCut];
-
 
 const dressSize = [xs, s, m, l, xl, xxl];
 const dressSizeWomen = [34, 36, 38, 40, 42, 44, 46, 48];
 const dressSizeMen = [44, 46, 48, 50, 52, 54];
-
-
-
-
